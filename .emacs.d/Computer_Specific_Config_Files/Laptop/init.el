b8c5d37e3dc069e5a95968ca685a018d6c8f3fbd
Mike Panitz <mike.panitz@gmail.com>
2019-08-13 14:10:43 -0700

    First attempt at storing my .emacs config

---
+++ .emacs.d/Computer_Specific_Config_Files/Laptop/init.el
@@ -0,0 +1,9 @@
+; contains info that will change between different physical computers
+; The goal is to allow for different directory structures on different computers
+
+(setq DROPBOX_ROOT "C:/MikesStuff/Pers/DropBox")
+(message (concat "\tDropbox root: " DROPBOX_ROOT))
+
+(setq STUDENT_WORK_ROOT "C:/MikesStuff/Work/Student_Work")
+(message (concat "\tWork root: " STUDENT_WORK_ROOT))
+
